20a36cfcb44235a6a8f6d70a264d8acb455e2e03
mystikalpotatoe <mystikalpotatoe@hotmail.com>
2017-07-03 07:02:38 +0100

    section 2 done

--- source/client.js
+++ source/client.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {default as Portfolio,PortfolioItem} from './components/portfolio.js';
 
 function hey(from='alex',to='mark') {
     return from + "to" + to
     }
 
 hey();
+
+ReactDOM.render(<Portfolio>
+					<PortfolioItem img="cake"/>
+					<PortfolioItem img="cabin"/>
+				</Portfolio>
+	,document.getElementById('react-app'));
